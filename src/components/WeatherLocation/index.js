733650f0e784c166447dd45fdcbf0547af59bcf9
Armando Ruiz <armando.ruiz@industrie.co>
2019-03-21 14:57:39 +1100

    Added OnClick handle and state

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -18,12 +18,39 @@ const data = {
   wind: '10 m/s',
 }
 
+const data2 = {
+  temperature: 35,
+  weatherState: SUN,
+  humidity: 90,
+  wind: '5 m/s',
+}
+
 class WeatherLocation extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      city: 'Sydney',
+      data: data, // coming from the const declared before.
+    };
+  }
+
+  handleUpdateClick =() => {
+    console.log("Actualizado");
+
+    this.setState({
+      city: 'Melbourne',
+      data: data2
+    });
+  }
+
   render () {
+    const { city, data } = this.state
     return (
       <div className="weatherLocationCont">
-        <Location city={"Maroubra"}/>
+        <Location city={city}/>
         <WeatherData data={data} />
+        <button onClick={this.handleUpdateClick}>Actualizar</button>
       </div>
     )
   }
