a36f340d4fe5892fefe4138985cf43d36b2d53ab
Armando Ruiz <armando.ruiz@industrie.co>
2019-03-21 13:53:33 +1100

    Double destructuring, Destructuring objetos complejos

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {
-  CLOUD,
-  CLOUDY,
-  SUN,
-  RAIN,
-  SNOW,
-  WINDY,
-} from './../../../constants/weather';
 import './styles.css';
 
-const WeatherData = () => (
-  <div className="weatherDataCont">
-    <WeatherTemperature temperature={20} weatherState={SNOW} />
-    <WeatherExtraInfo humidity={80} wind={"10 m/s"} />
-  </div>
-);
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind } }) => {
+  return(
+    <div className="weatherDataCont">
+      <WeatherTemperature
+        temperature={temperature}
+        weatherState={weatherState}
+        />
+      <WeatherExtraInfo humidity={humidity} wind={wind} />
+    </div>
+  )
+};
 
 export default WeatherData
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import {
+  CLOUD,
+  CLOUDY,
+  SUN,
+  RAIN,
+  SNOW,
+  WINDY,
+} from './../../constants/weather';
+
+const data = {
+  temperature: 5,
+  weatherState: SNOW,
+  humidity: 10,
+  wind: '10 m/s',
+}
 
 const WeatherLocation = () => (
   <div className="weatherLocationCont">
     <Location city={"Maroubra"}/>
-    <WeatherData />
+    <WeatherData data={data} />
   </div>
 );
 
