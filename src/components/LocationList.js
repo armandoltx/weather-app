357fe780f05f84605f6618d2fce4b2c332a16068
Armando Ruiz <armando.ruiz@industrie.co>
2019-03-26 10:55:33 +1100

    added media queries

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation';
+import './styles.css';
 
 
 const LocationList = ({ cities, onSelectedLocation }) => {
@@ -23,7 +24,7 @@ const LocationList = ({ cities, onSelectedLocation }) => {
 
   console.log(cities);
   return (
-    <div>
+    <div className="locationList">
       {strToComponents(cities)}
     </div>
   )
