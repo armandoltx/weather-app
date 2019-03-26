bf9ced1a0da95c598a0c1580f84f7ed246fa2557
Armando Ruiz <armando.ruiz@industrie.co>
2019-03-26 15:16:00 +1100

    Created indicador de carga inicial

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -19,17 +19,32 @@ const data = {
 }
 class ForecastExtended extends Component {
 
+  constructor() {
+    super();
+    this.state = { forecastData: null }
+  }
+
   renderForecastItemDays() {
     return days.map( day => (<ForecastItem weekDay={day} data={data}/>) );
   }
 
+  renderProgress = ()  => {
+    return <h3>Cargando Pronostico extendido...</h3>;
+  }
+
   render(){
     const { city } = this.props; // === const city = this.props.city;
+    const { forecastData } = this.state;
 
     return(
       <div>
         <h1 className='forecast-title'>Pronostico Extendido para {city} hour={10}</h1>
-        {this.renderForecastItemDays()}
+        {
+          forecastData ? 
+            this.renderForecastItemDays()
+          :
+            this.renderProgress()
+        }
       </div>
     );
   }
