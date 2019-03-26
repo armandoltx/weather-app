16302d024f5ac7dfc34bff643f25d885976fc46d
Armando Ruiz <armando.ruiz@industrie.co>
2019-03-26 14:31:21 +1100

    Created days array and applied to the forecastextended component

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import './styles.css';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
+import './styles.css';
 
+const days = [
+  'Lunes',
+  'Martes',
+  'Miercoles',
+  'Jueves',
+  'Viernes',
+];
 class ForecastExtended extends Component {
+
+  renderForecastItemDays() {
+    return days.map( day => (<ForecastItem weekDay={day}/>) );
+  }
+
   render(){
     const { city } = this.props; // === const city = this.props.city;
 
     return(
       <div>
         <h1 className='forecast-title'>Pronostico Extendido para {city}</h1>
-        <ForecastItem />
+        {this.renderForecastItemDays()}
       </div>
     );
   }
